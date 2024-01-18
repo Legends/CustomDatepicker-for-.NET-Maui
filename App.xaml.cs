b6be8857137808d73c3539dc59d5bc4d7a2d3b7d
Legends <jovan27@gmx.de>
2024-01-18 17:49:12 +0100

    --

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Maui.Handlers;
+﻿using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Handlers;
+using System.Globalization;
 
 namespace CustomDatepicker
 {
@@ -7,16 +9,18 @@ namespace CustomDatepicker
 		public App()
 		{
 			InitializeComponent();
- 
-            // default datepicker ignorant of date format and first day of week
-            var userLanguages = global::Windows.System.UserProfile.GlobalizationPreferences.Languages;
-            var dateFormat = new global::Windows.Globalization.DateTimeFormatting.DateTimeFormatter("shortdate", userLanguages);
-            DatePickerHandler.Mapper.AppendToMapping("FixDateFormat", (handler, view) =>
-            {
-                handler.PlatformView.FirstDayOfWeek = (global::Windows.Globalization.DayOfWeek)(int)
-                    System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
-                handler.PlatformView.DateFormat = dateFormat.Patterns.FirstOrDefault() ?? "shortdate";
-            }); 
+
+			CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("de-DE");
+
+			// default datepicker ignorant of date format and first day of week
+			var userLanguages = global::Windows.System.UserProfile.GlobalizationPreferences.Languages;
+			var dateFormat = new Windows.Globalization.DateTimeFormatting.DateTimeFormatter("shortdate", userLanguages);
+			DatePickerHandler.Mapper.AppendToMapping("FixDateFormat", (handler, view) =>
+			{
+				handler.PlatformView.FirstDayOfWeek = (Windows.Globalization.DayOfWeek)(int)
+					CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+				handler.PlatformView.DateFormat = dateFormat.Patterns.FirstOrDefault() ?? "shortdate";
+			});
 
 			MainPage = new AppShell();
 		}
--- Controls/MyDatepicker.xaml.cs
+++ Controls/MyDatepicker.xaml.cs
@@ -6,11 +6,11 @@ public partial class MyDatepicker : ContentView
 	{
 		InitializeComponent();
 	}
-	string text = "Pick a date";
+	string text = "Datum auswählen";
 	public string Text { get { return text; } set { text = value; OnPropertyChanged(); } }
  
 	private void OnDateSelected(object sender, DateChangedEventArgs e)
 	{
-		Text = e.NewDate.ToString("dd.MM.yyyy"); // your custom format or use IFormatProvider
+		Text = e.NewDate.ToShortDateString();// e.NewDate.ToString("dd.MM.yyyy"); // your custom format or use IFormatProvider
 	}
 }
