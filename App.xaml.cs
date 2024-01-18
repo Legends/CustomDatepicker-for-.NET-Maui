c0d32f7b02635cc323527750157259e554d7e94d
Legends <jovan27@gmx.de>
2024-01-18 11:56:45 +0100

    removing other platforms change first day of week

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace CustomDatepicker
+﻿using Microsoft.Maui.Handlers;
+
+namespace CustomDatepicker
 {
 	public partial class App : Application
 	{
 		public App()
 		{
 			InitializeComponent();
+ 
+            // default datepicker ignorant of date format and first day of week
+            var userLanguages = global::Windows.System.UserProfile.GlobalizationPreferences.Languages;
+            var dateFormat = new global::Windows.Globalization.DateTimeFormatting.DateTimeFormatter("shortdate", userLanguages);
+            DatePickerHandler.Mapper.AppendToMapping("FixDateFormat", (handler, view) =>
+            {
+                handler.PlatformView.FirstDayOfWeek = (global::Windows.Globalization.DayOfWeek)(int)
+                    System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+                handler.PlatformView.DateFormat = dateFormat.Patterns.FirstOrDefault() ?? "shortdate";
+            }); 
 
 			MainPage = new AppShell();
 		}
